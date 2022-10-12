6e0aa84215064c349b65a1359744803fd731213c
tangram <tangramteam@outlook.com>
2022-10-12 09:24:28 +0800

    107.0.5304.37

--- ChromiumSRC/src/content/browser/renderer_host/render_frame_host_impl.cc
+++ ChromiumSRC/src/content/browser/renderer_host/render_frame_host_impl.cc
@@ -7537,7 +7537,7 @@ void RenderFrameHostImpl::CreateNewWindow(
         std::make_unique<CrossOriginOpenerPolicyReporter>(
             GetProcess()->GetStoragePartition(), GetLastCommittedURL(),
             params->referrer->url, new_main_rfh->cross_origin_opener_policy(),
-            GetReportingSource(), isolation_info_.network_isolation_key()));
+            GetReportingSource(), isolation_info_.network_anonymization_key()));
   }
 
   mojo::PendingAssociatedRemote<mojom::Frame> pending_frame_remote;
@@ -10749,7 +10749,7 @@ void RenderFrameHostImpl::CreateWebTransportConnector(
   mojo::MakeSelfOwnedReceiver(
       std::make_unique<WebTransportConnectorImpl>(
           GetProcess()->GetID(), weak_ptr_factory_.GetWeakPtr(),
-          last_committed_origin_, isolation_info_.network_isolation_key()),
+          last_committed_origin_, isolation_info_.network_anonymization_key()),
       std::move(receiver));
 }
 
@@ -11136,7 +11136,7 @@ RenderFrameHostImpl::CreateNavigationRequestForSynchronousRendererCommit(
         storage_partition->GetWeakPtr(), url,
         cross_origin_embedder_policy().reporting_endpoint,
         cross_origin_embedder_policy().report_only_reporting_endpoint,
-        GetReportingSource(), isolation_info.network_isolation_key());
+        GetReportingSource(), isolation_info.network_anonymization_key());
   }
   std::unique_ptr<WebBundleNavigationInfo> web_bundle_navigation_info;
   if (is_same_document && web_bundle_handle_ &&
@@ -12283,7 +12283,7 @@ void RenderFrameHostImpl::MaybeGenerateCrashReport(
   // Send the crash report to the Reporting API.
   GetProcess()->GetStoragePartition()->GetNetworkContext()->QueueReport(
       /*type=*/"crash", /*group=*/"default", last_committed_url_,
-      GetReportingSource(), isolation_info_.network_isolation_key(),
+      GetReportingSource(), isolation_info_.network_anonymization_key(),
       absl::nullopt /* user_agent */, std::move(body));
 }
 
